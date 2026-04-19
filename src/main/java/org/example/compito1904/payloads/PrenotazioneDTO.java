c5cbc3c69750b8698007a21561322f7b66447eda
SergioMoschini <moschini2002@gmail.com>
2026-04-19 21:33:01 +0200

    tolti anche notblank su id e long

--- src/main/java/org/example/compito1904/payloads/PrenotazioneDTO.java
+++ src/main/java/org/example/compito1904/payloads/PrenotazioneDTO.java
@@ -1,14 +1,15 @@
 package org.example.compito1904.payloads;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record PrenotazioneDTO(
-        @NotBlank( message = "Campo obbligatorio")
+        @NotNull( message = "Campo obbligatorio")
         UUID idDipendente,
-        @NotBlank( message = "Campo obbligatorio")
+        @NotNull( message = "Campo obbligatorio")
         Long idViaggio,
         @Size( max = 150, message = "hai preferenze ?")
         String preferenze
